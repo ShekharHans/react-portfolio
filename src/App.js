8ac289bedd9e4da49125520c7a5bd555eb020702
Shekhar <shekharhans161@gmail.com>
2023-01-17 04:14:42 +0530

    -a

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Router, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 import Layout from './components/Layout'
 import Skills from './components/Skills'
-// import Project from './components/Project'
+
 import './App.scss'
-// import Project from './components/Project/Project'
+
 import Projects from "./components/Project/Projects";
 
 
 function App() {
   return (
     <>
+    <Router basename='/ReactPortfolio'>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route exact path="/"  element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/Project" element={<Projects/>} />
-          <Route path="/skills" element={<Skills />} />
+          <Route exact path="about" element={<About />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/Project" element={<Projects/>} />
+          <Route exact path="/skills" element={<Skills />} />
         </Route>
       </Routes>
+      </Router>
     </>
   )
 }
