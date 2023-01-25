50a4c620e6ee743b9b7a7bc4ea485b8d769c7399
Shekhar <shekharhans161@gmail.com>
2023-01-25 20:34:00 +0530

    -a

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import reportWebVitals from './reportWebVitals'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 // TODO:  Add mouse-trail, work page
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter basename=''>
       <App />
-    </BrowserRouter>
+    </HashRouter>
     ,
   </React.StrictMode>,
   document.getElementById('root')
